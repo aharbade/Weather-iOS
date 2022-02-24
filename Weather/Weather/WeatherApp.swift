3099b50a895812e57def75a11e156b1b6dd012b7
Anup Harbade <47821659+anupharbade89@users.noreply.github.com>
2022-02-24 03:08:01 -0600

    Adding the basic project structure

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Anup Harbade on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
